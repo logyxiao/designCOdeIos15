c2428db8e7af7c1a3257d1256299214fbfd22cff
yexiaojian <yexiaojian@binmade.cn>
2022-09-20 14:55:44 +0800

    Initial Commit

---
+++ designCOdeIos15/designCOdeIos15App.swift
@@ -0,0 +1,17 @@
+//
+//  designCOdeIos15App.swift
+//  designCOdeIos15
+//
+//  Created by to on 2022/9/20.
+//
+
+import SwiftUI
+
+@main
+struct designCOdeIos15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
